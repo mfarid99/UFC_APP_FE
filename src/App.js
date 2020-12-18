9bd045cf57d5e4920259f9c3425285693e7e8444
moe <moe.faridd@gmail.com>
2020-12-17 22:36:56 -0500

    commit 2: login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import Header from "./components/Header";
 import Signup from "./pages/signup"
+import Login from "./pages/login"
 import {Route, Link, Switch} from "react-router-dom";
 import './App.css';
 
@@ -10,17 +11,26 @@ export const GlobalCtx = React.createContext(null)
 
 
 function App() {
-  const [gState, setGState] = React.useState({ url: "http://localhost:5000"})
+  const [gState, setGState] = React.useState({ url: "http://localhost:5000", token: null})
+
+  React.useEffect(()=> {
+    const token = JSON.parse(window.localStorage.getItem("token"))
+    console.log(token)
+    if (token) {
+      setGState({...gState, token: token.token})
+    }
+  }, [])
+
   return (
     <GlobalCtx.Provider value = {{gState, setGState}}>
     <div className="App">
-      <h1>MMA</h1>
+      <Link to = "/" ><h1>MMA</h1></Link>
       <Header/>
       <main>
         <Switch>
           <Route exact path = "/" render={(rp)=> <h1>Home</h1>}/>
           <Route path = "/signup" render={(rp)=> <Signup {...rp}/>}/>
-          <Route path = "/login" render={(rp)=> <h1>Login</h1>}/>
+          <Route path = "/login" render={(rp)=> <Login {...rp}/>}/>
           <Route path = "/dashboard" render={(rp=> <h1>Dashboard</h1>)}/>
 
         </Switch>
