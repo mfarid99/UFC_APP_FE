6d97509466202805e5c1e4eb5b485b0ef43a71bb
moe <moe.faridd@gmail.com>
2020-12-24 17:13:02 -0500

    commit 11: fighters stats

--- src/pages/stats.js
+++ src/pages/stats.js
@@ -1,7 +1,8 @@
 import React from "react"
 import {GlobalCtx} from "../App"
-import {Figure, Button, Row} from 'react-bootstrap'
+import {Figure, Button, Row, Table} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 const Stats = (props) => {
@@ -10,6 +11,320 @@ const Stats = (props) => {
     return (<>
 
       <h1> Fighter stats</h1>
+
+      <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>First </th>
+      <th>Last </th>
+      <th>Nickname</th>
+      <th>Height</th>
+      <th>Weight</th>
+      <th>Reach</th>
+      <th>Wins</th>
+      <th>Losses</th>
+      <th>Draws</th>
+      <th>Belt</th>
+      <th>Average Oct. Time</th>
+      <th>Takedown Accuracy</th>
+      <th>Striking Accuracy</th>
+      <th>Win Finish %</th>
+
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+    <tr>
+      <td>Stephen</td>
+      <td>Thompson</td>
+      <td>Wonderboy</td>
+      <td>@6'0''</td>
+      <td>@170lbs.</td>
+      <td>75.0''</td>
+      <td>16</td>
+      <td>4</td>
+      <td>1</td>
+      <td></td>
+      <td>2:37</td>
+      <td>35%</td>
+      <td>72%</td>
+      <td>47%</td>
+    </tr>
+
+  </tbody>
+</Table>
 
       
     
